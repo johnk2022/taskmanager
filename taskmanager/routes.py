9d304dd216ce3233cac267eca83329d75fbf221e
johnk2022 <keenan.john@hotmail.com>
2022-08-26 15:42:44 +0000

    Initial commit

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,14 +1,8 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"):
-    import env  # noqa
+from flask import render_template
+from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-
-db = SQLAlchemy(app)
-
-from taskmanager import routes  # noqa
+@app.route("/")
+def home():
+    return render_template("base.html")
